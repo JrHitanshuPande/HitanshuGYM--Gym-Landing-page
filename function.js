fb7a53059484b55ab44fb301bc525749911d0c6a
JrHitanshuPande <116763099+JrHitanshuPande@users.noreply.github.com>
2023-07-12 19:15:26 +0530

    Nav for mobile is completed

--- function.js
+++ function.js
@@ -2,4 +2,22 @@
 function toggleNav() {
     const navList = document.getElementById("nav-list");
     navList.classList.toggle("active");
+
+    const iconr = document.querySelector(".iconbar");
+      iconr.style.display = "none";
+
+      
+    const iconx = document.querySelector(".iconbarx");
+    iconx.style.display = "block";
   }
+
+  //icon
+function toggleicon()
+{
+  const icont = document.querySelector(".iconbar");
+  icont.style.display="block";
+  const iconx = document.querySelector(".iconbarx");
+  iconx.style.display = "none";
+  const navList = document.getElementById("nav-list");
+  navList.classList.toggle("active");
+}
